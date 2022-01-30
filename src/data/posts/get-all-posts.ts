f77afa6343bccf1db65b1c21acc8910ef1b1e179
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-01-30 17:00:53 -0300

    Adding query on getAllPosts

--- src/data/posts/get-all-posts.ts
+++ src/data/posts/get-all-posts.ts
@@ -2,7 +2,9 @@ import { PostsData } from '../../domain/posts/post';
 import { fetchJson } from '../../utils/fetch-json';
 import { POSTS_URL } from '../../config/app-config';
 
-export async function getAllPosts(): Promise<PostsData> {
-  const posts = await fetchJson<PostsData>(POSTS_URL);
+export async function getAllPosts(query = ''): Promise<PostsData> {
+  const url = `${POSTS_URL}&${query}`;
+  console.log('url', url);
+  const posts = await fetchJson<PostsData>(url);
   return posts;
 }
