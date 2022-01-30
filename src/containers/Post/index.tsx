478f9da071b6fef9bf4e67eadac156332cef455f
Luiz Felipe <luizfelipedasilva678@gmail.com>
2022-01-30 19:10:35 -0300

    PostCover

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Heading } from '../../components/Heading';
 import { MainContainer } from '../../components/MainContainer';
+import { PostCover } from '../../components/PostCover';
 import { Post } from '../../domain/posts/post';
 
 export type PostProps = {
@@ -15,6 +16,11 @@ export const PostPage = ({ post }: PostProps): JSX.Element => {
       <MainContainer>
         <>
           <Heading>{post.attributes.title}</Heading>
+          <PostCover
+            alt={post.attributes.cover.data.attributes.formats.large.name}
+            coverUrl={post.attributes.cover.data.attributes.formats.large.url}
+          ></PostCover>
+
           <div
             dangerouslySetInnerHTML={{ __html: post.attributes.content }}
           ></div>
